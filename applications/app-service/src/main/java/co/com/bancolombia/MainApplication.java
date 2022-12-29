64480722f176890cc6043fd2df8c7a3b2646a16c
Alexander Pabon <alex312-@hotmail.com>
2022-12-28 21:50:39 -0500

    implements module applications

--- applications/app-service/src/main/java/co/com/bancolombia/MainApplication.java
+++ applications/app-service/src/main/java/co/com/bancolombia/MainApplication.java
@@ -1,9 +1,11 @@
 package co.com.bancolombia;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
--- applications/app-service/src/main/java/co/com/bancolombia/config/UseCasesConfig.java
+++ applications/app-service/src/main/java/co/com/bancolombia/config/UseCasesConfig.java
@@ -1,14 +1,18 @@
 package co.com.bancolombia.config;
 
+import co.com.bancolombia.model.customer.gateways.CustomerRepository;
+import co.com.bancolombia.usecase.customer.CustomerUseCase;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(basePackages = "co.com.bancolombia.usecase",
-        includeFilters = {
-                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
-        },
-        useDefaultFilters = false)
+@ComponentScan(basePackages = "co.com.bancolombia")
 public class UseCasesConfig {
+
+    @Bean
+    public CustomerUseCase customerUseCase(CustomerRepository customerRepository) {
+        return new CustomerUseCase(customerRepository);
+    }
 }
